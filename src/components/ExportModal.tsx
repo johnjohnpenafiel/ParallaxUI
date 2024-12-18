aa4817b08a2bdf5e9fc8bae68fe196e621a2af50
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-12-18 17:54:07 -0500

    add ExportModal.tsx

--- src/components/ExportModal.tsx
+++ src/components/ExportModal.tsx
@@ -15,7 +15,7 @@ const ExportModal = ({ open, onClose, embedCode }: ExportModalProps) => {
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          width: 400,
+          width: 600,
           bgcolor: "background.paper",
           boxShadow: 24,
           p: 4,
@@ -25,16 +25,50 @@ const ExportModal = ({ open, onClose, embedCode }: ExportModalProps) => {
         <Typography variant="h6" mb={2}>
           Export Code
         </Typography>
-        <TextField
-          value={embedCode}
-          fullWidth
-          multiline
-          slotProps={{
-            input: {
-              readOnly: true,
+        <Box
+          sx={{
+            maxHeight: 200,
+            overflow: "auto",
+            border: "1px solid #ccc",
+            borderRadius: 1,
+            p: 1,
+            scrollbarGutter: "stable",
+            "&::-webkit-scrollbar": {
+              width: "7px", // Visible scrollbar width
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#ccc", // Custom scrollbar appearance
+              borderRadius: "5px",
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              backgroundColor: "darkgray",
+            },
+            "&::-webkit-scrollbar-track": {
+              background: "transparent",
             },
           }}
-        />
+        >
+          <TextField
+            value={embedCode}
+            fullWidth
+            multiline
+            variant="outlined"
+            slotProps={{
+              input: {
+                readOnly: true,
+              },
+            }}
+            sx={{
+              "& .MuiInputBase-root": {
+                p: 0, // Remove input padding
+              },
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "none", // Remove text field border
+              },
+            }}
+          />
+        </Box>
+
         <Button
           variant="contained"
           color="secondary"
