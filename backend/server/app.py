94a5e5e753d668ffbd226a3c6e733529c9836f67
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-12-22 00:07:33 -0500

    PostgreSQL configured and ready, database initialized, env files created, app.py running

--- backend/server/app.py
+++ backend/server/app.py
@@ -4,12 +4,15 @@ from flask import Flask, request, make_response, session, jsonify
 from flask_migrate import Migrate
 from flask_cors import CORS
 from sqlalchemy import desc
+from dotenv import load_dotenv
 import os
 
 from models import db
 
+load_dotenv()
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get("DB_URI")
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
