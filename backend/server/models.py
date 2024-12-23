62839a9b6786126a0f7602d2e6c1c0278cd0ec29
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-12-22 23:00:49 -0500

    add Design class model

--- backend/server/models.py
+++ backend/server/models.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+from datetime import datetime
+
 convention = {
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -14,3 +16,15 @@ convention = {
 }
 
 db = SQLAlchemy(metadata=MetaData(naming_convention=convention))
+
+class Design(db.Model):
+    __tablename__ = 'design'
+
+    id = db.Column(db.Ineter, primary_key=True)
+    name = db.Column(db.String, nullable=True)
+    data = db.column(db.Text, nullable=False)
+    public_url = db.Column(db.String, unique=True, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.nowutc)
+
+    
